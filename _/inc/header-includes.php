7e75e7f2379d4d0ac09243169616531051bca68c
Anthony Keller <antkeller1@gmail.com>
2014-10-20 14:18:35 -0700

    Updated header includes to support staging site

--- _/inc/header-includes.php
+++ _/inc/header-includes.php
@@ -2,7 +2,11 @@
     // Figure out what our server name is
     $host = $_SERVER['SERVER_NAME'];
     // check if we are in the dev environment
-    if ($host == 'localhost' || $host == 'libtech.dev' || $host == 'libtech1415.staging.wpengine.com') {
+    if ($host == 'localhost' || $host == 'libtech.dev') {
+        // we're on dev, so include the dev CSS file and JavaScript individually for easier debugging
+        echo '<link href="' . get_template_directory_uri() . '/_/compiled/libtech.main.css" rel="stylesheet" type="text/css" />' . "\n";
+        include 'header-scripts.php';
+    } else if ($host == 'libtech1415.staging.wpengine.com') {
         // we're on dev, so include the dev CSS file and JavaScript individually for easier debugging
         echo '<link href="' . get_template_directory_uri() . '/_/compiled/libtech.main.css" rel="stylesheet" type="text/css" />' . "\n";
         include 'header-scripts.php';
