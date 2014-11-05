dcb775164be85deed0072612a09736b55b5073fe
Anthony Keller <antkeller1@gmail.com>
2014-11-04 16:20:03 -0800

    modified 404 page

--- 404.php
+++ 404.php
@@ -6,8 +6,23 @@
  */
  get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
-
-<?php get_sidebar(); ?>
+ 	<div class="bg3-top"></div>
+	<section class="bg3">
+		<div class="section-content">
+			<h2>404: We're working on it!</h2>
+			<div class="img-left-container clearfix">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/404/404-img-left.png" />
+			</div>
+			<div class="img-center-container clearfix">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/404/404-img-center.png" />
+			</div>
+			<div class="img-right-container clearfix">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/404/404-img-right.png" />
+			</div>
+			<div class="spacer clearfix">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/404/404-spacer.gif" /> 
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
