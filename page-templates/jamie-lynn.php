024529d41ce8e6b4fa7b762395e259ccdfd46567
Anthony Keller <antkeller1@gmail.com>
2014-11-11 12:21:16 -0800

    add initial Jamie Lynn micro-site build

--- page-templates/jamie-lynn.php
+++ page-templates/jamie-lynn.php
@@ -43,6 +43,8 @@ getRegionCode();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Misc. -->
     <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <script src="//use.typekit.net/ise4yoj.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
     <?php include get_template_directory() . '/_/inc/header-includes-jamie-lynn.php'; ?>
 	<!--[if lt IE 9]>
 		<script src="<?php bloginfo('template_directory'); ?>/_/js/lib/respond.min.js"></script>
@@ -50,12 +52,118 @@ getRegionCode();
     <!-- WordPress Head -->
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class('jamie-lynn'); ?>>
-	<div class="wrapper">
+<body <?php body_class(); ?>>
+	<div class="jamie-lynn">
+		<nav class="navigation">
+			<ul>
+				<li><a href="#then-now"></a></li>
+				<li><a href="#intro"></a></li>
+				<li><a href="#jami-video"></a></li>
+				<li><a href="#method"></a></li>
+				<li><a href="#front3"></a></li>
+				<li><a href="#art"></a></li>
+				<li><a href="#products"></a></li>
+				<li><a href="#signature"></a></li>
+			</ul>
+		</nav>
+		<section id="then-now" class="section-photo">
+			<div class="photo-item then-photo">
+				<div class="logo-wrapper">
+					<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/jamie-20-logo.png" alt="Jamie Lynn 20 year anniversary logo" />
+				</div>
+				<h4 class="mobile-message">Best view on wide screen</h4>
+				<h4 class="scroll-message">Scroll down</h4>
+			</div>
+		</section>
+		<section class="section-photo section-hide">
+			<div class="photo-item now-photo">
+				<div class="logo-wrapper">
+					<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/jamie-20-logo.png" alt="Jamie Lynn 20 year anniversary logo" />
+				</div>
+			</div>
+		</section>
+		<section id="intro" class="section-quote">
+			<div class="quote-item">
+				<div class="quote-wrapper">
+					<blockquote>
+						<p>he could have left, <span>he could have quit, </span><span>he could have done some weak ass shit, </span><span>but he doesn't know how</span></p>
+						<p>it's been over 20 years and,</p>
+						<p class="hand-written">he has done it all</p>
+					</blockquote>
+				</div>
+			</div>
+		</section>
+		<section id="jamie-video" class="section-video">
+			<div class="video-item">
+				<div class="video-wrapper">
+					<!-- <iframe src="" /> -->
+				</div>
+			</div>
+		</section>
+		<section id="method" class="section-quote">
+			<div class="quote-item">
+				<div class="quote-wrapper">
+					<blockquote class="quote-small">
+						<p>More than the gloveless fingers, spins off the toes, silhouetttes of florescent sirens and dirt rock dna, Jamie Lynn's method is his legacy. While every person who has taken flight on a snowboard has aspired to throw the perect method air, Jamie simply grabs between the bindings as if his board were a tool, instinctively extends his trailing leg with purpose and completes this economy of motion with his left arm providing a subtle counterpoint. To the rest of us it is perfect, but to Jamie it is just a method.</p>
+						<p class="hand-written">- Pat Bridges</p>
+					</blockquote>
+				</div>
+			</div>
+		</section>
+		<section class="section-photo">
+			<div class="photo-item road-gap-photo">
 
-
-
-	</div><!-- END .wrapper -->
+			</div>
+		</section>
+		<section id="front3" class="section-quote">
+			<div class="quote-item">
+				<div class="quote-wrapper">
+					<blockquote>
+						<p>Jamie style can be compared to true confidence. Confidence is silent. He lets the are of his actions speak for him.</p>
+						<p>No words required.</p>
+						<p class="hand-written">- Trevor Graves</p>
+					</blockquote>
+				</div>
+			</div>
+		</section>
+		<section class="section-photo">
+			<div class="photo-item front3-photo">
+			</div>
+		</section>
+		<section id="art" class="section-quote">
+			<div class="quote-item">
+				<div class="quote-wrapper">
+					<blockquote>
+						<p>He weaves his creativity through all that he does, inspiring men, women, and children to take life by the rat tail and live fast and loose.</p>
+						<p class="hand-written">- Barrett Christy</p>
+					</blockquote>
+				</div>
+			</div>
+		</section>
+		<section class="section-photo">
+			<div class="photo-item front3-photo">
+			</div>
+		</section>
+		<section id="products" class="section-products">
+			<div class="product-mobile">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/mobile/05-vertical-Jamie-product.jpg" alt="Jamie Lynn 20 year anniversary product photo" />
+			</div>
+			<div class="product-mobile-landscape">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/mobile/05-horizontal-Jamie-product.jpg" alt="Jamie Lynn 20 year anniversary product photo" />
+			</div>
+			<div class="product-tablet">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/tablet/05-vertical-Jamie-product.jpg" alt="Jamie Lynn 20 year anniversary product photo" />
+			</div>
+			<div class="product-desktop">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/tablet/05-horizontal-Jamie-product.jpg" alt="Jamie Lynn 20 year anniversary product photo" />
+			</div>
+		</section>
+		<section id="signature" class="section-signature">
+			<div class="signature-item">
+				<img src="<?php bloginfo('template_directory'); ?>/_/img/jamie-lynn/jamie-signature.png" alt="Jamie Lynn signature" />
+			</div>
+		</section>
+	</div><!-- END .jamie-lynn -->
 
 	<?php wp_footer(); ?>
 	
