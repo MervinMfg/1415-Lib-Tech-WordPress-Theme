d299550f24110bf06c194c3fa167e9ce333c7916
BrainBrian <brian@brainbrian.com>
2015-02-17 18:55:19 -0800

    Added form validation to second step within board finder application.

--- _/js/libtech.snowboard-finder.js
+++ _/js/libtech.snowboard-finder.js
@@ -1,6 +1,6 @@
 /**
- * 1415 Lib Tech WordPress Theme - Takeover - http://www.lib-tech.com
- * Author: brian.behrens@mervin.com - http://www.mervin.com
+ * 1415 Lib Tech WordPress Theme - Snowboard Finder - http://www.lib-tech.com
+ * Authors: brian.behrens@mervin.com & tony.keller@mervin.com - http://www.mervin.com
  */
 
 var LIBTECH = LIBTECH || {};
@@ -68,6 +68,31 @@ LIBTECH.BoardFinder.prototype = {
 		user.ability = "Starter";
 		user.terrain = "Freeride";
 	});
+	// DIRECTIVE TO CHECK FOR REQUIRED SELECT FIELDS
+	app.directive('requiredSelect', function() {
+		return {
+			restrict: 'A',
+			require: 'ngModel',
+			link: function(scope, elem, attr, ctrl) {
+				if (!ctrl) return;
+				attr.requiredSelect = true; // force truthy in case we are on non input element=
+				var validator = function(value) {
+					if (attr.requiredSelect && ctrl.$isEmpty(value) || attr.requiredSelect && value == -1) {
+						ctrl.$setValidity('requiredSelect', false);
+						return;
+					} else {
+						ctrl.$setValidity('requiredSelect', true);
+						return value;
+					}
+				};
+				ctrl.$formatters.push(validator);
+				ctrl.$parsers.unshift(validator);
+				attr.$observe('requiredSelect', function() {
+					validator(ctrl.$viewValue);
+				});
+			}
+		};
+	});
 	// STEP 1
 	app.controller('GenderController', function GenderController($scope, $routeParams, user) {
 		$scope.name = "GenderController";
@@ -78,7 +103,7 @@ LIBTECH.BoardFinder.prototype = {
 		};
 	});
 	// STEP 2
-	app.controller('SizeController', function SizeController($scope, $routeParams, $log, config, user) {
+	app.controller('SizeController', function SizeController($scope, $location, $routeParams, $log, config, user) {
 		$scope.name = "SizeController";
 		$scope.params = $routeParams;
 		$scope.config = config;
@@ -93,50 +118,48 @@ LIBTECH.BoardFinder.prototype = {
 			kg: -1
 		};
 		$scope.weightImperial = [
-			{label: 'Pounds', value: -1}, {label: '40 lbs', value: 40}, {label: '50 lbs', value: 50}, {label: '60 lbs', value: 60},
-			{label: '70 lbs', value: 70}, {label: '80 lbs', value: 80}, {label: '90 lbs', value: 90}, {label: '100 lbs', value: 100},
-			{label: '110 lbs', value: 110}, {label: '120 lbs', value: 120}, {label: '130 lbs', value: 130}, {label: '140 lbs', value: 140},
-			{label: '150 lbs', value: 150}, {label: '160 lbs', value: 160}, {label: '170 lbs', value: 170}, {label: '180 lbs', value: 180},
-			{label: '190 lbs', value: 190}, {label: '200 lbs', value: 200}, {label: '210 lbs', value: 210}, {label: '220 lbs', value: 220},
-			{label: '230 lbs', value: 230}, {label: '240 lbs', value: 240}, {label: '250 lbs', value: 250}, {label: '260 lbs', value: 260},
-			{label: '260+ lbs', value: 270}
+			{label: '40 lbs', value: 40}, {label: '50 lbs', value: 50}, {label: '60 lbs', value: 60}, {label: '70 lbs', value: 70},
+			{label: '80 lbs', value: 80}, {label: '90 lbs', value: 90}, {label: '100 lbs', value: 100}, {label: '110 lbs', value: 110},
+			{label: '120 lbs', value: 120}, {label: '130 lbs', value: 130}, {label: '140 lbs', value: 140}, {label: '150 lbs', value: 150},
+			{label: '160 lbs', value: 160}, {label: '170 lbs', value: 170}, {label: '180 lbs', value: 180}, {label: '190 lbs', value: 190},
+			{label: '200 lbs', value: 200}, {label: '210 lbs', value: 210}, {label: '220 lbs', value: 220}, {label: '230 lbs', value: 230},
+			{label: '240 lbs', value: 240}, {label: '250 lbs', value: 250}, {label: '260 lbs', value: 260}, {label: '260+ lbs', value: 270}
 		];
 		$scope.weightMetric = [
-			{label: 'Kilograms', value: -1}, {label: '20 kg', value: 20}, {label: '25 kg', value: 25}, {label: '30 kg', value: 30},
-			{label: '35 kg', value: 35}, {label: '40 kg', value: 40}, {label: '45 kg', value: 45}, {label: '50 kg', value: 50},
-			{label: '55 kg', value: 55}, {label: '60 kg', value: 60}, {label: '65 kg', value: 65}, {label: '70 kg', value: 70},
-			{label: '75 kg', value: 75}, {label: '80 kg', value: 80}, {label: '85 kg', value: 85}, {label: '90 kg', value: 90},
-			{label: '95 kg', value: 95}, {label: '100 kg', value: 100}, {label: '105 kg', value: 105}, {label: '110 kg', value: 110},
-			{label: '115 kg', value: 115}, {label: '120 kg', value: 120}, {label: '120+ kg', value: 125}
+			{label: '20 kg', value: 20}, {label: '25 kg', value: 25}, {label: '30 kg', value: 30}, {label: '35 kg', value: 35},
+			{label: '40 kg', value: 40}, {label: '45 kg', value: 45}, {label: '50 kg', value: 50}, {label: '55 kg', value: 55},
+			{label: '60 kg', value: 60}, {label: '65 kg', value: 65}, {label: '70 kg', value: 70}, {label: '75 kg', value: 75},
+			{label: '80 kg', value: 80}, {label: '85 kg', value: 85}, {label: '90 kg', value: 90}, {label: '95 kg', value: 95},
+			{label: '100 kg', value: 100}, {label: '105 kg', value: 105}, {label: '110 kg', value: 110}, {label: '115 kg', value: 115},
+			{label: '120 kg', value: 120}, {label: '120+ kg', value: 125}
 		];
 		$scope.heightFeet = [
-			{label: 'Feet', value: -1}, {label: "2'", value: 2}, {label: "3'", value: 3}, {label: "4'", value: 4},
-			{label: "5'", value: 5}, {label: "6'", value: 6}, {label: "7'", value: 7}
+			{label: "2'", value: 2}, {label: "3'", value: 3}, {label: "4'", value: 4}, {label: "5'", value: 5},
+			{label: "6'", value: 6}, {label: "7'", value: 7}
 		];
 		$scope.heightInches = [
-			{label: 'Inches', value: -1}, {label: '0"', value: 0}, {label: '1"', value: 1}, {label: '2"', value: 2},
-			{label: '3"', value: 3}, {label: '4"', value: 4}, {label: '5"', value: 5}, {label: '6"', value: 6},
-			{label: '7"', value: 7}, {label: '8"', value: 8}, {label: '9"', value: 9}, {label: '10"', value: 10},
-			{label: '11"', value: 11}
+			{label: '0"', value: 0}, {label: '1"', value: 1}, {label: '2"', value: 2}, {label: '3"', value: 3},
+			{label: '4"', value: 4}, {label: '5"', value: 5}, {label: '6"', value: 6}, {label: '7"', value: 7},
+			{label: '8"', value: 8}, {label: '9"', value: 9}, {label: '10"', value: 10}, {label: '11"', value: 11}
 		];
 		$scope.heightMetric = [
-			{label: 'Centimeters', value: -1}, {label: "60 cm", value: 60}, {label: "65 cm", value: 65}, {label: "70 cm", value: 70},
-			{label: "75 cm", value: 75}, {label: "80 cm", value: 80}, {label: "85 cm", value: 85}, {label: "90 cm", value: 90},
-			{label: "95 cm", value: 95}, {label: "100 cm", value: 100}, {label: "105 cm", value: 105}, {label: "110 cm", value: 110},
-			{label: "115 cm", value: 115}, {label: "120 cm", value: 120}, {label: "125 cm", value: 125}, {label: "130 cm", value: 130},
-			{label: "135 cm", value: 135}, {label: "140 cm", value: 140}, {label: "145 cm", value: 145}, {label: "150 cm", value: 150},
-			{label: "155 cm", value: 155}, {label: "160 cm", value: 160}, {label: "165 cm", value: 165}, {label: "170 cm", value: 170},
-			{label: "175 cm", value: 175}, {label: "180 cm", value: 180}, {label: "185 cm", value: 185}, {label: "190 cm", value: 190},
-			{label: "195 cm", value: 195}, {label: "200 cm", value: 200}, {label: "205 cm", value: 205}, {label: "210 cm", value: 210},
-			{label: "215 cm", value: 215}, {label: "220 cm", value: 220}
+			{label: "60 cm", value: 60}, {label: "65 cm", value: 65}, {label: "70 cm", value: 70}, {label: "75 cm", value: 75},
+			{label: "80 cm", value: 80}, {label: "85 cm", value: 85}, {label: "90 cm", value: 90}, {label: "95 cm", value: 95},
+			{label: "100 cm", value: 100}, {label: "105 cm", value: 105}, {label: "110 cm", value: 110}, {label: "115 cm", value: 115},
+			{label: "120 cm", value: 120}, {label: "125 cm", value: 125}, {label: "130 cm", value: 130}, {label: "135 cm", value: 135},
+			{label: "140 cm", value: 140}, {label: "145 cm", value: 145}, {label: "150 cm", value: 150}, {label: "155 cm", value: 155},
+			{label: "160 cm", value: 160}, {label: "165 cm", value: 165}, {label: "170 cm", value: 170}, {label: "175 cm", value: 175},
+			{label: "180 cm", value: 180}, {label: "185 cm", value: 185}, {label: "190 cm", value: 190}, {label: "195 cm", value: 195},
+			{label: "200 cm", value: 200}, {label: "205 cm", value: 205}, {label: "210 cm", value: 210}, {label: "215 cm", value: 215},
+			{label: "220 cm", value: 220}
 		];
 		$scope.bootSizes = [
-			{label: 'Size', value: -1}, {label: 4, value: 4}, {label: 4.5, value: 4.5}, {label: 5, value: 5},
-			{label: 5.5, value: 5.5}, {label: 6, value: 6}, {label: 6.5, value: 6.5}, {label: 7, value: 7},
-			{label: 7.5, value: 7.5}, {label: 8, value: 8}, {label: 8.5, value: 8.5}, {label: 9, value: 9},
-			{label: 9.5, value: 9.5}, {label: 10, value: 10}, {label: 10.5, value: 10.5}, {label: 11, value: 11},
-			{label: 11.5, value: 11.5}, {label: 12, value: 12}, {label: 12.5, value: 12.5}, {label: 13, value: 13},
-			{label: 13.5, value: 13.5}, {label: 14, value: 14}, {label: 14.5, value: 14.5}, {label: 15, value: 15}
+			{label: 4, value: 4}, {label: 4.5, value: 4.5}, {label: 5, value: 5}, {label: 5.5, value: 5.5},
+			{label: 6, value: 6}, {label: 6.5, value: 6.5}, {label: 7, value: 7}, {label: 7.5, value: 7.5},
+			{label: 8, value: 8}, {label: 8.5, value: 8.5}, {label: 9, value: 9}, {label: 9.5, value: 9.5},
+			{label: 10, value: 10}, {label: 10.5, value: 10.5}, {label: 11, value: 11}, {label: 11.5, value: 11.5},
+			{label: 12, value: 12}, {label: 12.5, value: 12.5}, {label: 13, value: 13}, {label: 13.5, value: 13.5},
+			{label: 14, value: 14}, {label: 14.5, value: 14.5}, {label: 15, value: 15}
 		];
 		// check values on init of controller
 		if($scope.user.weight != -1) {
@@ -193,9 +216,13 @@ LIBTECH.BoardFinder.prototype = {
 			}
 			$scope.user.height = updatedHeight;
 		}
+		function setSize() {
+			$location.path( '/style/' );
+		}
 		// make methods public
 		$scope.changeWeight = changeWeight;
 		$scope.changeHeight = changeHeight;
+		$scope.setSize = setSize;
 	});
 	// STEP 3
 	app.controller('StyleController', function StepThreeController($scope, $routeParams) {
