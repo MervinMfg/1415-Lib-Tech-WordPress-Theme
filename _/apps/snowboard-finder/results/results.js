d64b8da9cfbeea87ea21c222ea73869f76bc0f46
BrainBrian <brian@brainbrian.com>
2015-03-04 13:39:26 -0800

    Update results display for snowboard finder
    • results now tile based on the latest feed
    • display colorways with snowboards

--- _/apps/snowboard-finder/results/results.js
+++ _/apps/snowboard-finder/results/results.js
@@ -22,11 +22,11 @@
 		$scope.config = config;
 
 		angular.forEach($scope.config.snowboards, function(item, i) {
-			$log.info(i + " - " + item.title);
+			/*$log.info(i + " - " + item.title);
 			$log.log(item.link, item.slug);
 			$log.log(item.meta.libtech_product_price_us, item.meta.libtech_product_price_ca, item.meta.libtech_product_price_eur);
 			$log.log("prod image id: " + item.meta.libtech_product_image);
-			$log.log("--------------------------------");
+			$log.log("--------------------------------");*/
 		});
 		// BOOT SIZES
 		// OUR MODIFIED CHART
