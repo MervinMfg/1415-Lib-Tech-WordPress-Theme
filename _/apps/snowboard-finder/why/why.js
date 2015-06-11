acbafc4f220183e275dfb22e22d697840fccf609
BrainBrian <brian@brainbrian.com>
2015-06-11 16:30:42 -0700

    Update Why Board Finder step to work with updated user provider
    - add resolve data check in config ot make sure we have all the needed data
    - update calls to user provider

--- _/apps/snowboard-finder/why/why.js
+++ _/apps/snowboard-finder/why/why.js
@@ -6,13 +6,30 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('boardFinder.why', ['ngRoute', 'ngAnimate', 'boardFinder.contour']);
+	var app = angular.module('boardFinder.why', ['ngRoute', 'ngAnimate', 'boardFinder.contour', 'boardFinder.user']);
 
-	app.config(['$routeProvider', function($routeProvider) {
+	app.config(['$routeProvider', 'userProvider', function($routeProvider, userProvider) {
 		$routeProvider.when('/why/', {
 			templateUrl: '/wp-content/themes/1415-Lib-Tech-WordPress-Theme/_/apps/snowboard-finder/why/why.html',
 			controller: 'WhyController',
-			controllerAs: 'whyCtrl'
+			controllerAs: 'whyCtrl',
+			resolve: {
+				dataCheck: ['$location', function ($location) {
+					if(userProvider.checkGender()) {
+						if(userProvider.checkSize()) {
+							if(userProvider.checkStyle()) {
+								return true;
+							} else {
+								$location.path('/style/');
+							}
+						} else {
+							$location.path('/size/');
+						}
+					} else {
+						$location.path('/');
+					}
+				}]
+			}
 		});
 	}]);
 
@@ -28,19 +45,19 @@
 
 		function init() {
 			// set
-			if($scope.user.weight != -1) {
-				$scope.lbs = Math.round($scope.user.weight * 2.20462 / 10) * 10; // convert kg to lbs, round to nearest 10 lbs
+			if($scope.user.weight() != -1) {
+				$scope.lbs = Math.round($scope.user.weight() * 2.20462 / 10) * 10; // convert kg to lbs, round to nearest 10 lbs
 			}
 			// set imperial measurements
-			if($scope.user.height != -1) {
-				$scope.feet = Math.floor($scope.user.height/30.48);
-				$scope.inches = Math.round(($scope.user.height/2.54) % 12);
+			if($scope.user.height() != -1) {
+				$scope.feet = Math.floor($scope.user.height()/30.48);
+				$scope.inches = Math.round(($scope.user.height()/2.54) % 12);
 			}
 		}
 
 		function checkContour($contour) {
-			for(var i=0; i < $scope.user.contours.length; i++){
-				if($scope.user.contours[i] === $contour){
+			for(var i=0; i < $scope.user.contours().length; i++){
+				if($scope.user.contours()[i] === $contour){
 					return true;
 				}
 			}
